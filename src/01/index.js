0c495967d3f1a8a4bcca21578d83a03a4f59045a
juicecultus <juicecultus@gmail.com>
2020-07-12 19:24:12 +0100

    Exercise 01 - solution using switch / case

--- src/01/index.js
+++ src/01/index.js
@@ -3,23 +3,42 @@ const elBox = document.querySelector('#box');
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
-    state
+  switch (state) {
     // Add your state/event transitions here
     // to determine and return the next state
-  ) {
+    case 'inactive':
+      switch (event) {
+        case 'CLICK':
+          return 'active';
+        default:
+          return state;
+      }
+    case 'active':
+      switch (event) {
+        case 'CLICK':
+          return 'inactive';
+        default:
+          return state;
+      }
+    default:
+      return state;
   }
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = 'inactive';
 
 function send(event) {
   // Determine the next value of `currentState`
+  const nextState = transition(currentState, event);
 
+  currentState = nextState;
+
+  // [data-state='inactive']
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
   // send a click event
+  send('CLICK');
 });
