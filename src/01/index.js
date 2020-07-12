e7e76bd657f62ac4470c4a699bfaf76d837df4ee
juicecultus <juicecultus@gmail.com>
2020-07-12 19:29:52 +0100

    Exercise 01 - solution using state machine

--- src/01/index.js
+++ src/01/index.js
@@ -1,28 +1,25 @@
 const elBox = document.querySelector('#box');
 
+const machine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        CLICK: 'active',
+      },
+    },
+    active: {
+      on: {
+        CLICK: 'inactive',
+      },
+    },
+  },
+};
+
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (state) {
-    // Add your state/event transitions here
-    // to determine and return the next state
-    case 'inactive':
-      switch (event) {
-        case 'CLICK':
-          return 'active';
-        default:
-          return state;
-      }
-    case 'active':
-      switch (event) {
-        case 'CLICK':
-          return 'inactive';
-        default:
-          return state;
-      }
-    default:
-      return state;
-  }
+  return machine.states[state].on[event] || state;
 }
 
 // Keep track of your current state
@@ -31,6 +28,8 @@ let currentState = 'inactive';
 function send(event) {
   // Determine the next value of `currentState`
   const nextState = transition(currentState, event);
+
+  console.log(nextState);
 
   currentState = nextState;
 
